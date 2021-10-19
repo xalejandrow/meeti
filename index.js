d97289e87188be3ea5d8f59e9faf3ddec846a699
Alew <xalejandrow@gmail.com>
2021-10-19 18:27:33 -0300

    Cap. 188. Creando las primeras páginas y creando nuestro middleware

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expresLayouts = require('express-ejs-layouts');
 const path = require('path');
-const routes = require('./routes');
+const router = require('./routes');
 
 require('dotenv').config({ path: 'variables.env'});
 
@@ -17,9 +17,15 @@ app.set('views', path.join(__dirname, './views'));
 // archivos estáticos
 app.use(express.static('public'));
 
+// Middleware (usuario logueado, flash messages, fecha actual)
+app.use((req, res, next) => {
+    const fecha = new Date();
+    res.locals.year = fecha.getFullYear();
+    next();
+});
 
 // Routing
-app.use('/', routes());
+app.use('/', router());
 
 // Agrega el puerto
 app.listen(process.env.PORT, () => {
